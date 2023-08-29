4d4f3fde0bb1c7906e1fff04975467c02cbf4304
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-08-29 11:36:35 +0100

    Added animation to the testimonial card

--- lib/responsive/desktop/sections/testimonials/widgets/testimonial_card.dart
+++ lib/responsive/desktop/sections/testimonials/widgets/testimonial_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TestimonialCard extends StatelessWidget {
+class TestimonialCard extends StatefulWidget {
   final String image;
   final String name;
   const TestimonialCard({
@@ -12,72 +12,133 @@ class TestimonialCard extends StatelessWidget {
   });
 
   @override
+  State<TestimonialCard> createState() => _TestimonialCardState();
+}
+
+class _TestimonialCardState extends State<TestimonialCard>
+    with TickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<Color?> _forwardColorAnimation;
+  late final Animation<Color?> _reverseColorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 800,
+      ),
+    );
+
+    _forwardColorAnimation = ColorTween(
+      begin: const Color.fromARGB(255, 95, 95, 95),
+      end: const Color(0xff1B1B1B).withOpacity(0),
+    ).animate(_animationController);
+
+    _reverseColorAnimation = ColorTween(
+      begin: const Color(0xff1B1B1B).withOpacity(0),
+      end: const Color.fromARGB(255, 95, 95, 95),
+    ).animate(_animationController);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        CustomPaint(
-          // size: const Size(445, 212),
-          painter: TestimonialBoxPainter(),
-          child: Container(
-            width: 445,
-            height: 212,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30,
-              vertical: 30,
-            ),
-            child: Column(
-              children: [
-                Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-                  style: GoogleFonts.ibmPlexMono(
-                    color: const Color(0xff9C9C9C),
-                    fontSize: 14,
-                    height: 24 / 14,
+    return InkWell(
+      onTap: () {},
+      onHover: (value) {
+        if (value) {
+          if (_animationController.isAnimating) {
+            _animationController.reset();
+          }
+          _animationController.forward();
+        } else {
+          if (_animationController.isCompleted ||
+              _animationController.isAnimating) {
+            _animationController.reverse();
+          }
+        }
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          AnimatedBuilder(
+            animation: Listenable.merge([
+              _forwardColorAnimation,
+              _reverseColorAnimation,
+            ]),
+            builder: (context, child) {
+              return CustomPaint(
+                painter: TestimonialBoxPainter(
+                  begin: _forwardColorAnimation.value!,
+                  end: _reverseColorAnimation.value!,
+                ),
+                child: Container(
+                  width: 445,
+                  height: 212,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 30,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                        style: GoogleFonts.ibmPlexMono(
+                          color: const Color(0xff9C9C9C),
+                          fontSize: 14,
+                          height: 24 / 14,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 25,
+                            backgroundImage: AssetImage(widget.image),
+                          ),
+                          const SizedBox(width: 20),
+                          Text(
+                            widget.name,
+                            style: GoogleFonts.raleway(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
-                const SizedBox(height: 30),
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 25,
-                      backgroundImage: AssetImage(image),
-                    ),
-                    const SizedBox(width: 20),
-                    Text(
-                      "Chinedu Jeremiah",
-                      style: GoogleFonts.raleway(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              );
+            },
+          ),
+          Positioned(
+            top: -10,
+            left: 10,
+            child: SvgPicture.asset(
+              'icons/approstrophe.svg',
             ),
           ),
-        ),
-        Positioned(
-          top: -10,
-          left: 10,
-          child: SvgPicture.asset(
-            'icons/approstrophe.svg',
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
 
 class TestimonialBoxPainter extends CustomPainter {
+  final Color begin;
+  final Color end;
+
+  TestimonialBoxPainter({
+    required this.begin,
+    required this.end,
+  });
   @override
   void paint(Canvas canvas, Size size) {
     LinearGradient strokeColor = LinearGradient(
-      colors: [
-        const Color(0xff484848),
-        const Color(0xff1B1B1B).withOpacity(0),
-      ],
+      colors: [begin, end],
       begin: Alignment.centerLeft,
       end: Alignment.centerRight,
     );
@@ -104,6 +165,6 @@ class TestimonialBoxPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
